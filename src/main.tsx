346673e1c18667916abcc4cd2d4de89957f73d4e
Geoffrey Burt <geoffreyjburt@gmail.com>
2025-06-27 23:21:45 -0400

    Setup main.tsx with QueryCLientProvider and ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -8,10 +6,18 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
