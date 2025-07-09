70133e4b966afee184f8b6190cd14fb366931081
Geoffrey Burt <geoffreyjburt@gmail.com>
2025-07-09 01:23:08 -0400

    Installed firebase and setup config.ts with firebase config data. The firebase project has been created with Authentication for Email and Firebase Store API.

---
+++ src/firebase/config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// Firebase configuration for the Advanced E-commerce App
+// This configuration is used to initialize Firebase services in the application
+// In real world production applications with sensitive data such as API keys would be stored securely using a .env file or similar method
+// However, for portfolio project purposes, I am directly including it here.
+const firebaseConfig = {
+    apiKey: "AIzaSyB8F7X63eGUvpKMwkBIDHS8zW7qKuNqKu4",
+    authDomain: "advanced-ecommerce-app-9663a.firebaseapp.com",
+    projectId: "advanced-ecommerce-app-9663a",
+    storageBucket: "advanced-ecommerce-app-9663a.appspot.com",
+    messagingSenderId: "880241815482",
+    appId: "1:880241815482:web:56b10b01cd519165a2b783"
+};
+// Initialize Firebase with the provided configuration
+const app = initializeApp(firebaseConfig);
+export default app;
