c439a12a0be9dcc1a192571445feebc4599d6790
Geoffrey Burt <geoffreyjburt@gmail.com>
2025-07-21 03:32:43 -0400

    Attempting to use logic that identifies Front End use with Vercel and Back end use with GitHub Actions.

--- src/firebase/getFirebaseConfig.ts
+++ src/firebase/getFirebaseConfig.ts
@@ -1,13 +1,13 @@
 const getFirebaseConfig = () => {
-    const isTestEnv = typeof process !== 'undefined' && process.env?.CI === 'true';
+    const isBrowser = typeof window !== 'undefined';
 
     return {
-        apiKey: isTestEnv ? process.env.VITE_FIREBASE_API_KEY! : import.meta.env.VITE_FIREBASE_API_KEY,
-        authDomain: isTestEnv ? process.env.VITE_FIREBASE_AUTH_DOMAIN! : import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-        projectId: isTestEnv ? process.env.VITE_FIREBASE_PROJECT_ID! : import.meta.env.VITE_FIREBASE_PROJECT_ID,
-        storageBucket: isTestEnv ? process.env.VITE_FIREBASE_STORAGE_BUCKET! : import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: isTestEnv ? process.env.VITE_FIREBASE_MESSAGING_SENDER_ID! : import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-        appId: isTestEnv ? process.env.VITE_FIREBASE_APP_ID! : import.meta.env.VITE_FIREBASE_APP_ID,
+        apiKey: isBrowser ? import.meta.env.VITE_FIREBASE_API_KEY : process.env.VITE_FIREBASE_API_KEY!,
+        authDomain: isBrowser ? import.meta.env.VITE_FIREBASE_AUTH_DOMAIN : process.env.VITE_FIREBASE_AUTH_DOMAIN!,
+        projectId: isBrowser ? import.meta.env.VITE_FIREBASE_PROJECT_ID : process.env.VITE_FIREBASE_PROJECT_ID!,
+        storageBucket: isBrowser ? import.meta.env.VITE_FIREBASE_STORAGE_BUCKET : process.env.VITE_FIREBASE_STORAGE_BUCKET!,
+        messagingSenderId: isBrowser ? import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID : process.env.VITE_FIREBASE_MESSAGING_SENDER_ID!,
+        appId: isBrowser ? import.meta.env.VITE_FIREBASE_APP_ID : process.env.VITE_FIREBASE_APP_ID!,
     };
 };
 
